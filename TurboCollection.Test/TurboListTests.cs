9dc06d0cb0fa5e7171cb755b7293c2fb951e4cd8
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2022-01-18 00:46:58 +0100

    Add new test and Array

--- TurboCollection.Test/TurboListTests.cs
+++ TurboCollection.Test/TurboListTests.cs
@@ -14,13 +14,39 @@ namespace TurboCollection.Test
         Assert.Zero(list.Count);
     }
 
-    //[Test]
-    //public void AddingAnElementIncreasesCountToOne()
-    //{
-        //var list = new TurboList<int>();
-        //list.Add(5);
-        //Assert.AreEqual(1, list.Count);
-    //}
+    [Test]
+    public void AddingAnElementIncreasesCountToOne()
+    {
+        var list = new TurboList<int>();
+        list.Add(5);
+        Assert.AreEqual(1, list.Count);
+    }
+    
+    
+    [Test, TestCase(5), TestCase(7)]
+    public void AddingMultipleElementsIncreasesTheCount(int numberOfElements)
+    {
+        var list = new TurboList<int>();
+        for (int i = 0; i < numberOfElements; i++)
+            list.Add(5);
+       
+        Assert.AreEqual(numberOfElements, list.Count);
+    }
+
+    [Test]
+    public void AnAddedElementCanBeGet()
+    {
+        var list = new TurboList<int>();
+        list.Add(1337);
+        Assert.AreEqual(1337, list.Get(0));
+
+
+
+    }
+    
+    
+    
+    
     
   } 
 
--- TurboCollections/TurboList.cs
+++ TurboCollections/TurboList.cs
@@ -4,11 +4,33 @@ namespace TurboCollections{
 
 public class TurboList<T>
 {
-    public int Count => 0;
+    public int Count => items.Length;
+    private T[] items = Array.Empty<T>();
     public TurboList()
     {
         
         Console.WriteLine("Hello, Turbo!");
+    }
+
+    public void Add(T item)
+    {    // Resize the array
+        T[] newArray = new T[Count + 1];
+        for (int i = 0; i < Count; i++)
+        {
+            newArray[i] = items[i];
+        }
+        
+        //Assign the new element
+        newArray[Count] = item;
+       
+        //Assign the result to our field
+        items = newArray;
+
+    }
+
+    public T Get(int index)
+    {
+        return items[index];
     }
 } 
 
