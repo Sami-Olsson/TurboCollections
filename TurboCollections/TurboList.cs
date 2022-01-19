20ab04670832eb290c8a50fa638929a116aade0e
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2022-01-19 21:04:24 +0100

    EnsureSize method

--- TurboCollections/TurboList.cs
+++ TurboCollections/TurboList.cs
@@ -4,7 +4,7 @@ namespace TurboCollections{
 
 public class TurboList<T>
 {
-    public int Count => items.Length;
+    public int Count { get; private set; }
     private T[] items = Array.Empty<T>();
     public TurboList()
     {
@@ -19,15 +19,27 @@ public class TurboList<T>
         {
             newArray[i] = items[i];
         }
-        
+        items = newArray;
         //Assign the new element
         newArray[Count] = item;
-       
-        //Assign the result to our field
-        items = newArray;
+        
+        Count++;
 
     }
 
+    void EnsureSize(int size)
+    {   
+        if(items.Length >= size)
+            return;
+        int newSize = Math.Max(size, items.Length * 2);
+
+        T[] newArray = new T[Count + 1];
+        for (int i = 0; i < Count; i++)
+        {
+            newArray[i] = items[i];
+        }
+        items = newArray;
+    }
     public T Get(int index)
     {
         return items[index];
