883f8f9c8edd9b8e2fe87dfecb94ac08a05fb907
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2022-01-19 21:22:30 +0100

    MultipleAnAddedElementCanBeGotten

--- TurboCollection.Test/TurboListTests.cs
+++ TurboCollection.Test/TurboListTests.cs
@@ -23,7 +23,7 @@ namespace TurboCollection.Test
     }
     
     
-    [Test, TestCase(5), TestCase(7)]
+    [TestCase(5), TestCase(7)]
     public void AddingMultipleElementsIncreasesTheCount(int numberOfElements)
     {
         var list = new TurboList<int>();
@@ -34,17 +34,26 @@ namespace TurboCollection.Test
     }
 
     [Test]
-    public void AnAddedElementCanBeGet()
+    public void AnAddedElementCanBeGotten()
     {
         var list = new TurboList<int>();
         list.Add(1337);
         Assert.AreEqual(1337, list.Get(0));
-
-
-
     }
     
-    
+    [Test]
+    public void MultipleAnAddedElementCanBeGotten()
+    {
+        int[] numbers = {5, 7, -12, 9, 3, -4, 104, 12};
+        var list = new TurboList<int>();
+        foreach(var number in numbers)
+                list.Add(number);
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Assert.AreEqual(numbers[i], list.Get(i));
+        }
+        
+    }
     
     
     
--- TurboCollections/TurboList.cs
+++ TurboCollections/TurboList.cs
@@ -13,31 +13,31 @@ public class TurboList<T>
     }
 
     public void Add(T item)
-    {    // Resize the array
-        T[] newArray = new T[Count + 1];
-        for (int i = 0; i < Count; i++)
-        {
-            newArray[i] = items[i];
-        }
-        items = newArray;
-        //Assign the new element
-        newArray[Count] = item;
+    {    
+        EnsureSize(Count + 1);
+        items[Count++] = item;
         
-        Count++;
-
     }
-
+    /// <summary>
+    /// This method ensures that the array is at least 'size' big
+    /// </summary>
+    /// <param name="size">The size that your array should have</param>
     void EnsureSize(int size)
     {   
+        // if the array is large enough, return
         if(items.Length >= size)
             return;
+        
+        //double the array size, or set it to given size if doubling is not enough
         int newSize = Math.Max(size, items.Length * 2);
-
+        
+        //create new array
         T[] newArray = new T[Count + 1];
         for (int i = 0; i < Count; i++)
-        {
+        {   //copy old items
             newArray[i] = items[i];
         }
+        // assign new array
         items = newArray;
     }
     public T Get(int index)
