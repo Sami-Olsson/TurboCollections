faf5ae8f5ebca0b2e616597cfbb8bd449644bcfa
Sami-Olsson <70694914+Sami-Olsson@users.noreply.github.com>
2022-01-23 18:41:52 +0100

    ANewStack+Setup

--- TurboCollection.Test/TurboStackTests.cs
+++ TurboCollection.Test/TurboStackTests.cs
@@ -1,10 +1,33 @@
 ﻿using NUnit.Framework;
+using TurboCollections;
 
 namespace TurboCollection.Test
 {
     [TestFixture]
     public class TurboStackTests
     {
+        [Test]
+        public void HasEmptyConstuctor()
+        {
+            new TurboStack<int>();
+            Assert.Pass();
+        } 
         
+        public class ANewStack
+        {
+            private TurboStack<int> stack;
+
+            [SetUp]
+            public void Setup()
+            {
+                this.stack = new TurboStack<int>();
+            }
+            
+            [Test]
+            public void HasAcountOfZero()
+            {
+                
+            }
+        }
     }
 }
